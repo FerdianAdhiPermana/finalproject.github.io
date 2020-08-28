1a7432d97010b157e39e75f3e430918a7c06196c
FerdianAdhiPermana <adhipermanaferdian@gmail.com>
2020-08-28 16:53:52 +0700

    ferdianadhipermana

---
+++ index.js
@@ -0,0 +1,6 @@
+// function myFunction() {
+//     var x = document.getElementById("myImg").src;
+//     document.getElementById("demo").innerHTML = x;
+//   }
+
+var image = document.getElementById("")
